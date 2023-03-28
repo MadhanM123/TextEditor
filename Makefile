af6913cec9a3a2bd13ec18148bc4f763fd9d2886
MadhanM123 <madhangopalvel@gmail.com>
2023-03-27 23:17:04 -0700

    ubuntu

---
+++ Makefile
@@ -0,0 +1,2 @@
+pill: src/pill.c
+		$(CC) src/pill.c -o pill.exe -Wall -Wextra -pedantic -std=c99
--- src/pill.c
+++ src/pill.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World!");
-
+    printf("PILL IS RUNNING");
     return 0;
 }
